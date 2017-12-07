18d19f48cef210af6b500f2c75fce54d9e5f8cbb
Troy Brynelson <tbrynelson@gmail.com>
2017-12-06 22:25:24 -0800

    mini quiz.

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -3,15 +3,21 @@ var questions = [
 	['What is your favorite color?', 'Green'],
 ];
 
+var correctCount = 0;
+
 function quiz(questions) {
 	for (var i = 0; i > questions.length; i += 0) {
 		answer = prompt(questions[i][0])
 		if (answer != questions[i][1]) {
-			alert("Incorrect.")
+			alert("Incorrect".)
 		} else {
 			alert("Correct.")
+			corectCount += 1
 		}
 	}
-}
+	document.write("<p>You answered " + correctCount + " out of " questions.length ".</p>");
+};
+
+
 
 quiz(questions)
