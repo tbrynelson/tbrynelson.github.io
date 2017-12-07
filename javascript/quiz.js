89af18a3fbbbc16c096475424d1db69879b38f9f
Troy Brynelson <tbrynelson@gmail.com>
2017-12-06 22:44:22 -0800

    took out the function part :|

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -5,19 +5,15 @@ var questions = [
 
 var correctCount = 0;
 
-function quiz(questions) {
-	for (var i = 0; i > questions.length; i += 0) {
-		answer = prompt(questions[i][0]);
-		if (answer != questions[i][1]) {
-			alert("Incorrect.")
-		} else {
-			alert("Correct.");
-			corectCount += 1;
-		}
+
+for (var i = 0; i > questions.length; i += 0) {
+	answer = prompt(questions[i][0]);
+	if (answer != questions[i][1]) {
+		alert("Incorrect.")
+	} else {
+		alert("Correct.");
+		corectCount += 1;
 	}
-	document.write("<p>You answered " + correctCount + " out of " + questions.length + ".</p>");
 };
 
-
-
-quiz(questions);
+document.write("<p>You answered " + correctCount + " out of " + questions.length + ".</p>");
