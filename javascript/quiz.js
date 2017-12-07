f42ef8d7b8277f6c4fe6a97be7eaf0633e187b47
Troy Brynelson <tbrynelson@gmail.com>
2017-12-06 23:59:59 -0800

    this is a doozy and probably filled with errors

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -4,6 +4,8 @@ var questions = [
 	['What is Troy\'s girlfriend\'s name?', 'Lacey'],
 	['Does Troy have a cat or a dog?', 'Cat'],
 ];
+var incorrectAnswers = [];
+var correctAnswers = [];
 
 var correctCount = 0;
 var question;
@@ -11,19 +13,38 @@ var answer;
 var response;
 var html;
 
+
 function print(message) {
 	var outputDiv = document.getElementById('output');
 	outputDiv.innerHTML = message;
 }
+
+function buildList(arr) {
+	var listHTML = '<ol>';
+	for (var i = 0; i < arr.length; i += 1) {
+		listHTML += '<li>' + arr[i] + '</li>';
+	}
+	listHTML += "</ol>";
+	return listHTML
+	}
 
 for (var i = 0; i < questions.length; i += 1) {
 	question = questions[i][0];
 	answer = questions[i][1];
 	response = prompt(question).toLowerCase();
 	if ( response === answer.toLowerCase() ) {
-		correctCount += 1
+		correctCount += 1;
+		correctAnswers.push(question);
+
+	} else {
+		incorrectAnswers.push(question);
 	}
 };
 
-html = "<p>You answered " + correctCount + " out of " + questions.length + ".</p>";
+html = "<p>You answered " + correctCount + " out of " + questions.length + ".</p>"
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(correctAnswers);
+html += "<h2>You got these questions wrong:</h2>";
+html += buildList(incorrectAnswers);
+
 print(html)
