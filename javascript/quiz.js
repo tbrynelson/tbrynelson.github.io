d9ac59ef670b931b5fa63ec172e164e50acc96d3
Troy Brynelson <tbrynelson@gmail.com>
2017-12-06 23:05:41 -0800

    lowercase

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -18,8 +18,8 @@ function print(message) {
 for (var i = 0; i < questions.length; i += 1) {
 	question = questions[i][0];
 	answer = questions[i][1];
-	response = prompt(question).lower();
-	if ( response === answer.lower() ) {
+	response = prompt(question).toLowerCase();
+	if ( response === answer.toLowerCase() ) {
 		correctCount += 1
 	}
 }
