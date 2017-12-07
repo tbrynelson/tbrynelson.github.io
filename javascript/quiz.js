8039738205a2efee551c8a623c933c547d1eca16
Troy Brynelson <tbrynelson@gmail.com>
2017-12-06 22:36:40 -0800

    token again

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -9,7 +9,7 @@ function quiz(questions) {
 	for (var i = 0; i > questions.length; i += 0) {
 		answer = prompt(questions[i][0]);
 		if (answer != questions[i][1]) {
-			alert("Incorrect".)
+			alert("Incorrect.")
 		} else {
 			alert("Correct.");
 			corectCount += 1;
