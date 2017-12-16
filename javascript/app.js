708e9f308f33b951d7815ca4174cc9927e5b07af
Troy Brynelson <tbrynelson@gmail.com>
2017-12-16 12:10:12 -0800

    test test
    test

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,7 @@
 const myHeading = document.getElementById("myHeading");
 const myButton = document.getElementById("myButton");
+const myTextInput = document.getElementById("myTextInput");
 
 myButton.addEventListener('click', () => {
-	myHeading.style.color = 'red';
+	myHeading.style.color = myTextInput.value;
 });
