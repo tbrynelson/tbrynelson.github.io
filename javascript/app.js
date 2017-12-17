677a672b8d3edfb1b5907d438f7b41645c259f1e
Troy Brynelson <tbrynelson@gmail.com>
2017-12-16 17:20:22 -0800

    Hiding info

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,15 @@
+const toggleInfo = document.getElementById('toggleInfo');
+const infoDiv = document.querySelector('#info');
 const input = document.querySelector('input');
 const p = document.querySelector('p.description');
 const button = document.querySelector('button');
 
+toggleInfo.addEventListener('click', () => {
+	infoDiv.style.display = 'none';
+});
+
 button.addEventListener('click', () => {
-	p.textContent = input.value + ';';
+	p.innerHTML = input.value + ':';
 })
+
+p.title = "List description";
