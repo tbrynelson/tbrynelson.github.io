39d8b1678ee8ea702e10b208fb7be9657fe022f7
Troy Brynelson <tbrynelson@gmail.com>
2017-12-20 23:27:28 -0800

    I don't know wtf happened.

--- javascript/app.js
+++ javascript/app.js
@@ -17,9 +17,19 @@ infoDiv.addEventListener('mouseout', () => {
 
 listUl.addEventListener('click', (event) => {
 	if (event.target.tagName == 'BUTTON') {
+		if (event.target.className == 'remove') {
 		let li = event.target.parentNode;
 		let ul = li.parentNode;
 		ul.removeChild(li);
+		}
+		if (event.target.className == 'up') {
+		let li = event.target.parentNode;
+		let prevLi = li.previousElementSibling;
+		let ul = li.parentNode;
+		if (prevLi) {
+			ul.insertBefore(li, prevLi);
+			}
+		}
 	}
 })
 
