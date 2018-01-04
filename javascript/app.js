1d14e0a5ae1566717f26bba43cfec41474420409
Troy Brynelson <tbrynelson@gmail.com>
2018-01-03 22:31:12 -0800

    JAVERSCREEP

--- javascript/app.js
+++ javascript/app.js
@@ -1,90 +1,12 @@
 const toggleTests = document.getElementById('toggleTests');
 const testDiv = document.querySelector('#testGround');
-const input = document.querySelector('input.description');
-const p = document.querySelector('p.description');
-const descriptionButton = document.querySelector('button.description');
-const addItemInput = document.querySelector('input.addItemInput');
-const addItemButton = document.querySelector('button.addItemButton');
-const listUl = testDiv.querySelector('ul');
-const lis = listUl.children;
 
-function attachListItemButtons(li) {
-	let up = document.createElement('button');
-	up.className = 'up';
-	up.textContent = 'Up';
-	li.appendChild(up);
-	let down = document.createElement('button');
-	down.className = 'down';
-	down.textContent = 'Down';
-	li.appendChild(down);
-	let remove = document.createElement('button');
-	remove.className = 'remove';
-	remove.textContent = 'Remove';
-	li.appendChild(remove);
-}
-
-/*infoDiv.addEventListener('mouseover', () => {
-	listItems.textContent = listItems.textContent.toUpperCase();
-});
-
-infoDiv.addEventListener('mouseout', () => {
-	listItems.textContent = listItems.textContent.toLowerCase();
-});*/
-
-for (let i = 0; i < lis.length; i += 1) {
-	attachListItemButtons(lis[i]);
-}
-
-listUl.addEventListener('click', (event) => {
-	if (event.target.tagName == 'BUTTON') {
-		if (event.target.className == 'remove') {
-		let li = event.target.parentNode;
-		let ul = li.parentNode;
-		ul.removeChild(li);
-		}
-		if (event.target.className == 'up') {
-			let li = event.target.parentNode;
-			let prevLi = li.previousElementSibling;
-			let ul = li.parentNode;
-		if (prevLi) {
-			ul.insertBefore(li, prevLi);
-			}
-		}
-		if (event.target.className == 'down') {
-			let li = event.target.parentNode;
-			let nextLi = li.nextElementSibling;
-			let ul = li.parentNode;
-			if (nextLi) {
-				ul.insertBefore(nextLi, li);
-			}
-		}
+toggleTests.addEvenetListener('click', () => {
+	if (testDiv.style.display == 'none') {
+		toggleTests.textContent = 'Hide.';
+		testDiv.style.display = 'block';
+	} else {
+		toggleTests.textContent = "Show tests.";
+		testDiv.style.display = 'none';
 	}
-})
-
-document.addEventListener('click', (event) => {
-	console.log(event.target);
-});
-
-toggleTests.addEventListener('click', () => {
-if (testDiv.style.display == 'none') {
-	toggleTests.textContent = 'Hide info';
-	testDiv.style.display = 'block';
-} else {
-	toggleTests.textContent = 'Show info';
-	testDiv.style.display = 'none';
-}
-});
-
-descriptionButton.addEventListener('click', () => {
-	descriptionP.innerHTML = descriptionInput.value + ':';
-	descriptionInput.value = '';
-});
-
-addItemButton.addEventListener('click', () => {
-	let ul = document.getElementsByTagName('ul')[0];
-	let li = document.createElement('li');
-	li.textContent = addItemInput.value;
-	attachListItemButtons(li);
-	ul.appendChild(li);
-	addItemInput.value = '';
 });
