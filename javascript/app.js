cb711ea7e7f2d33e8503fde89e1f119808360dd2
Troy Brynelson <tbrynelson@gmail.com>
2018-01-04 21:39:10 -0800

    Try this
    Fix the double click for the button I guess

--- javascript/app.js
+++ javascript/app.js
@@ -2,11 +2,11 @@ const toggleTests = document.getElementById('toggleTests');
 const testLinks = document.querySelector('.testGround');
 
 toggleTests.addEventListener('click', () => {
-	if (testLinks.style.display == 'none') {
-		toggleTests.textContent = 'Hide.';
-		testLinks.style.display = 'block';
-	} else {
+	if (testLinks.style.display == 'block') {
 		toggleTests.textContent = 'Show.';
 		testLinks.style.display = 'none';
+	} else {
+		toggleTests.textContent = 'Hide.';
+		testLinks.style.display = 'block';
 	}
 });
