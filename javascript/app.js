96704c013791b28eedc78f13f49ce7dae7272e20
Troy Brynelson <tbrynelson@gmail.com>
2018-01-07 12:57:31 -0800

    Added some jQuery animation to the button, changed the testing area to an unordered list.
    OK!

--- javascript/app.js
+++ javascript/app.js
@@ -4,9 +4,9 @@ const testLinks = document.querySelector('.testGround');
 toggleTests.addEventListener('click', () => {
 	if (testLinks.style.display == 'block') {
 		toggleTests.textContent = 'Show.';
-		testLinks.style.display = 'none';
+		$(testLinks).slideUp(100);
 	} else {
 		toggleTests.textContent = 'Hide.';
-		testLinks.style.display = 'block';
+		$(testLinks).slideDown(100);
 	}
 });
