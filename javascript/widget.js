eb99aa9466ec57f0279e487ee34ece65c59543a8
Troy Brynelson <tbrynelson@gmail.com>
2018-01-15 22:43:27 -0800

    New rooms
    OK!

--- javascript/widget.js
+++ javascript/widget.js
@@ -18,3 +18,24 @@ xhr.onreadystatechange = function () {
 };
 xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+const roomSon = new XMLHttpRequest();
+roomSon.onreadystatechange = function () {
+	if (roomSon.readystate === 4) {
+		const rooms = JSON.parse(roomSon.responseText);
+		const roomsHTML = '<ul class="rooms">';
+		for (var i = 0; i < rooms.length; i += 1) {
+			if (rooms[i].available === true) {
+				roomsHTML += '<li class="full">';
+			} else {
+				roomsHTML += '<li class="empty">';
+			}
+		}	roomsHTML += rooms[i].room;
+			roomsHTML += '</li>';
+	}
+	roomsHTML += '</ul>';
+	document.getElementById('roomList').innerHTML = roomsHTML;
+}
+
+roomSon.open('GET', '../data/rooms.json');
+roomSon.send();
