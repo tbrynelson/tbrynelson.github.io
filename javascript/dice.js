f583d178dd05687ef77d0e9e6d8bb866e46428c2
Troy Brynelson <tbrynelson@gmail.com>
2018-01-26 01:18:30 -0800

    yah

--- javascript/dice.js
+++ javascript/dice.js
@@ -5,3 +5,13 @@ var dice = {
 		return randomNumber
 	}
 }
+
+function Dice(sides) {
+	this.sides = sides;
+	this.roll = function () {
+		const randomNumber = Math.floor(Math.random() * this.sides) + 1;
+		return randomNumber
+	}
+}
+
+var dice = new Dice(6);
---
+++ javascript/playlistapp.js
@@ -0,0 +1,29 @@
+var playlist = new Playlist()
+
+var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
+var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves", "3:43");
+var spaceOddity = new Song("Space Oddity", "David Bowie", "5:15");
+var rocketMan = new Song("Rocket Man", "Elton John", "4:42");
+
+playlist.add(hereComesTheSun);
+playlist.add(walkingOnSunshine);
+playlist.add(spaceOddity);
+playlist.add(rocketMan);
+
+
+var playlistElement = document.getElementById("playlist")
+
+playlist.renderInElement(playlistElement);
+
+var playButton = document.getElementById("play");
+playButton.onclick = function() {
+	playlist.play()
+}
+var nextButton = document.getElementById("next");
+nextButton.onclick = function() {
+	playlist.next()
+}
+var stopButton = document.getElementById("stop");
+stopButton.onclick = function() {
+	playlist.stop()
+}
